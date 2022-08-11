d43c8bf77aa980aafb9b9ad3b4a5be98beebdd7e
MarioHaz <mariohazbun@gmail.com>
2022-08-11 07:35:59 -0500

    working

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+const Home = () => {
+    return (
+     <h1>REACT-SHOP</h1>
+    );
+}
+
+export default Home;
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const NotFound = () => {
+    return (
+        <div>
+            <p>not found</p>
+        </div>
+    );
+}
+
+export default NotFound;
